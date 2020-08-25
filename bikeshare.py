9cf9cc45f5f01a4d8354da9aefd0b3e503bbd900
Mahmoud Salah <mahmoud.s.nafi@gmail.com>
2020-08-26 00:34:15 +0200

    Second Commit

--- bikeshare.py
+++ bikeshare.py
@@ -22,17 +22,17 @@ def get_filters():
         month=input("which month between januaray and june you want to filter by? insert 'all' for no filter: ").lower()
 
         day=input("which weekday you want to filter by? insert 'all' for no filter: ").lower()
-        
+
         _valid_cities=['chicago', 'new york city','washington']
         _valid_months=['january','february','march''april','may','June','june','all']
         _valid_days=['saturday','sunday','monday','tuesday','wednesday','thursday','friday','all']
-        
+
         if city in _valid_cities and month in _valid_months and day in _valid_days:
             break
         else:
             print('\nInvalid inputs, Please insert them again correctly.\n')
             continue
-    
+
     print('-'*40)
     return city, month, day
 
@@ -53,7 +53,7 @@ def load_data(city, month, day):
     df['month']=df['Start Time'].dt.month
     df['day']=df['Start Time'].dt.weekday_name
     df['hour']=df['Start Time'].dt.hour
-    
+
     if month !='all':
         months=['january', 'february', 'march', 'april', 'may', 'june']
         month=months.index(month)+1
@@ -61,7 +61,7 @@ def load_data(city, month, day):
 
     if day !='all':
         df=df[df['day']==day.title()]
-           
+
     return df
 
 
@@ -72,7 +72,7 @@ def time_stats(df):
     start_time = time.time()
 
     months=['january', 'february', 'march', 'april', 'may', 'june']
-    
+
     time_stats_df=pd.DataFrame({'Time Stats':[months[df['month'].mode()[0]-1],df['day'].mode()[0],df['hour'].mode()[0]]},
                                index=['Most Common Month','Most Common Weekday','Most Common Hour'])
     print(time_stats_df)
@@ -86,7 +86,7 @@ def station_stats(df):
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
-    
+
     df['start-end-stations']=df['Start Station']+' --> '+df['End Station']
 
     popular_stations_df=pd.DataFrame({'Popular Stations and Trips':
@@ -102,16 +102,16 @@ def trip_duration_stats(df):
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
-    
+
     total_duration=df['Trip Duration'].sum()
     avg_duration=df['Trip Duration'].mean()
-    
+
     duration_df=pd.DataFrame({'Duration in seconds':[total_duration,avg_duration],
                               'Duration in minutes':[total_duration/60,avg_duration/60],
                               'Duration in hours':[total_duration/3600,avg_duration/3600]},
                              index=['Total','Average'])
     print(duration_df)
-                
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -140,16 +140,36 @@ def user_stats(df,city):
     print('-'*40)
 
 
+def raw_data(df):
+    ques=input("\nDo you want to see how your raw data looks like, yes or no? ").lower()
+    counter=0
+    if ques=='yes':
+        print(df.iloc[counter:counter+5])
+        while True:
+            another_ques=input("\nDo you want to see another 5 rows, yes or no? ").lower()
+            if another_ques=='yes':
+                counter+=5
+                print(df.iloc[counter:counter+5])
+            elif another_ques=='no':
+                break
+            else:
+                print("\nInvalid input, please insert yes or no")
+                continue
+    elif ques=='no':
+        pass
+    else:
+        print("\nInvalid input, please insert yes or no")
+        raw_data(df)
+
 def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-        print("That is how your data looks like!\n") 
-        print(df.head(10))
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df,city)
+        raw_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
